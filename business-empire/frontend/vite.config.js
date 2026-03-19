fd045b6a1f249ce52d6154dc5faa606283398612
Makar732 <kanaev.makar.1202@gmail.com>
2026-03-19 20:54:56 +0300

    Update vite.config.js

--- business-empire/frontend/vite.config.js
+++ business-empire/frontend/vite.config.js
@@ -4,6 +4,14 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  // ============================
+  // При сборке (build) файлы попадут в frontend/dist
+  // backend/index.js раздаёт эту папку
+  // ============================
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true
+  },
   server: {
     port: 5173,
     proxy: {
